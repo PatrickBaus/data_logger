b982d959b44f275be26eb990e53f801ea6dab1ac
Patrick Baus <github@maat-family.de>
2025-12-23 14:40:23 +0100

    Feat/add usb vid pid (#52)
    * Add option to use VID/PID combination to select serial devices
    
    * Updated example to use the VID/PID syntax

--- devices/async_serial.py
+++ devices/async_serial.py
@@ -19,7 +19,17 @@
 import asyncio
 import logging
 
+import serial.tools.list_ports
 import serial_asyncio
+
+
+def find_device_by_vid(vid: int, pid: int, serial_number: str | None = None) -> str | None:
+    for port in serial.tools.list_ports.comports():
+        if port.vid == vid and port.pid == pid:
+            if serial_number is not None and serial_number != port.serial_number:
+                continue
+            return port.device
+    return None
 
 
 class AsyncSerial:
@@ -35,8 +45,18 @@ class AsyncSerial:
     def is_connected(self):
         return self.__writer is not None and not self.__writer.is_closing()
 
-    def __init__(self, tty, separator=b"\n", timeout=None, **kwargs):
-        self.__tty = tty
+    def __init__(
+        self,
+        tty: str | None = None,
+        vid_pid: tuple[int, int] | None = None,
+        serial_number: str | None = None,
+        separator=b"\n",
+        timeout=None,
+        **kwargs,
+    ):
+        self.__tty = find_device_by_vid(*vid_pid, serial_number=serial_number) if vid_pid is not None else tty
+        if self.__tty is None:
+            raise ValueError("Either supply a tty string or a VID/PID tuple.")
         self.__separator = separator
         self.__lock = None
         self.__kwargs = kwargs
